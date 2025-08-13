e3a040e4717f0625f5029f18f0929bb5091a28a1
kindablueski <xavieralexander5200@gmail.com>
2025-08-13 13:38:18 -0700

    add leetcode tags

--- src/main/java/com/develeveling/backend/entity/LeetCodeProblemSolve.java
+++ src/main/java/com/develeveling/backend/entity/LeetCodeProblemSolve.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data @Entity @NoArgsConstructor
 @Table(name = "leetcode_solves",
@@ -24,4 +26,8 @@ public class LeetCodeProblemSolve {
     private LocalDate firstSolvedAt;
 
     public enum Difficulty { EASY, MEDIUM, HARD }
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "leetcode_solve_tags", joinColumns = @JoinColumn(name = "solve_id"))
+    @Column(name = "tag")
+    private Set<String> tags = new HashSet<>();
 }
